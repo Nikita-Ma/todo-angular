8a43f30c18f08d25cbadc593084e66608da67c10
Nik <nikitama1hr@gmail.com>
2023-06-06 23:58:40 +0300

    create new module and use his

--- my-first-angular-app/src/app/app.module.ts
+++ my-first-angular-app/src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TestModule} from "./test/test.module";
+// @NgModule es6 module
+// declarations. es6 let/const
+// imports. es6 let/const
+// exports. es6 let/const
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TestModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
