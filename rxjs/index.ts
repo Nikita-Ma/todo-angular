3aed41510fd644ad9af4f5a1c91be1558cb35845
Nik <nikitama1hr@gmail.com>
2023-06-10 15:51:25 +0300

    first rxjs

--- rxjs/index.ts
+++ rxjs/index.ts
@@ -1,8 +1,29 @@
-const sequence = new Promise((res) => {
-    let count = 0;
-    setInterval(() => {
-        res(count++)
-    })
-})
+// const sequence = new Promise((res) => {
+//     let count = 0;
+//     setInterval(() => {
+//         res(count++)
+//     })
+// })
+//
+// sequence.then((v)=>console.log(v))
 
-sequence.then((v)=>console.log(v))
+
+//======================================================
+
+// const sequence = function* iteraotrFn() {
+//     let item = 1
+//     while (true) {
+//         yield item++
+//     }
+// }()
+//
+//
+// console.log(sequence.next().value)
+
+// =====================================================
+
+import {interval} from "rxjs";
+
+interval(1000).subscribe((v) => {
+    console.log(v)
+})
