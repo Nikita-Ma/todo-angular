0c8f2d8c4ad18c54d5854182ba67490e5902bd98
Nik <nikitama1hr@gmail.com>
2023-06-10 16:49:59 +0300

    hot & cold streams

--- rxjs/index.ts
+++ rxjs/index.ts
@@ -1,20 +1,17 @@
-// ReactiveX - iterator + observer
+import {Observable} from "rxjs";
 
-class CustomIterable {
-
-    private cursor = 0
-    private value: number
-
-    constructor(private arr: number[], private divistor = 1) {
-    }
-
-
-    private next() {
-        while (this.cursor < this.arr.length) {
-            this.value = this.arr[this.cursor++]
-            if (this.value % this.divistor === 0) {
-                return {done: false, value: this.value}
+const exampleObserever$ = new Observable(subscriber => {
+        let count = 1
+        const int = setInterval(() => {
+            if (count % 5 === 0) {
+                clearInterval(int)
+                subscriber.complete()
+                return
             }
-        }
+            subscriber.next(count++)
+        }, 3000)
     }
-}
+)
+exampleObserever$.subscribe((v) => {
+    console.log(v)
+}, (error)=>{}, (complite)=>{})
