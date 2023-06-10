f16bc995949ee6dfe34a51c31b01a9468f1cdcfd
Nik <nikitama1hr@gmail.com>
2023-06-10 11:35:18 +0300

    basic router

--- my-first-angular-app/src/app/app-routing.module.ts
+++ my-first-angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ModalComponent} from "./modal/modal.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'qwe',
+    component: ModalComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
