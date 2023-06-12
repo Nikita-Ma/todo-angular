550d3bbdc79786ed14cd6f83bb0f9cf80b35f56a
Nik <nikitama1hr@gmail.com>
2023-06-12 08:49:15 +0300

    Directive

--- my-second-angular-app/src/app/app.module.ts
+++ my-second-angular-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColoryDirective } from './colory.directive';
 import { ChildComponent } from './child/child.component';
+import { DelayDirective } from './delay.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ColoryDirective,
-    ChildComponent
+    ChildComponent,
+    DelayDirective
   ],
   imports: [
     BrowserModule,
