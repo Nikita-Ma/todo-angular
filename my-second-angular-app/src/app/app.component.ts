ab7d745aee44ae5b1b6f26bca98376d2d9e3914f
Nik <nikitama1hr@gmail.com>
2023-06-12 08:27:41 +0300

    @Input && @Attribute

--- my-second-angular-app/src/app/app.component.ts
+++ my-second-angular-app/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-second-angular-app';
+    protected readonly InputEvent = InputEvent;
 }
--- my-second-angular-app/src/app/app.module.ts
+++ my-second-angular-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColoryDirective } from './colory.directive';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ColoryDirective
+    ColoryDirective,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
