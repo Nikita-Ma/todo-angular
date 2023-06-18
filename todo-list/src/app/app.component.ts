c79e2d52c87af22d77d3dce48c8f17f7a41e6954
Nik <nikitama1hr@gmail.com>
2023-06-18 18:16:35 +0300

    change to important and delete

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+
+  public todolist = [
+    {
+      index: 0,
+      name: 'Drink coffee',
+      important: false,
+      complete: false
+    },
+    {
+      index: 1,
+      name: 'Sleep',
+      important: true,
+      complete: false
+    },
+  ]
+
+  reImportant(event: any, index: any) {
+    this.todolist[index].important = !this.todolist[index].important
+  }
+
+  delItem(event:any, index:any){
+    delete this.todolist[index]
+  }
+
+
+  protected readonly JSON = JSON;
 }
